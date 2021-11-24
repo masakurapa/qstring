7f51a56d9a8a33822517f3cef890e05e9ce25b1c
masakurapa <rabasan1031@gmail.com>
2021-11-24 22:44:38 +0900

    update encoder

--- encoder.go
+++ encoder.go
@@ -6,65 +6,84 @@ import (
 	"reflect"
 )
 
-// Q is the type of the query string parameters
+// Q is the type of the query string parameters.
 type Q map[string]interface{}
 
-// ArrayQ is a type of query string in array format
+// ArrayQ is a type of query string in array format.
 type ArrayQ []interface{}
 
-// MapQ is a type of query string in map format
+// MapQ is a type of query string in map format.
 type MapQ map[string]interface{}
 
-// Encode returns the URL-encoded query string
+// Encode returns the URL-encoded query string.
 //
-// add "?" to the beginning and return
-func (q *Q) Encode() (string, error) {
-	if len(*q) == 0 {
+// supports arguments of type map.
+//
+// add "?" to the beginning and return.
+func Encode(v interface{}) (string, error) {
+	e := encoder{v: url.Values{}}
+	r := reflect.ValueOf(v)
+
+	switch r.Kind() {
+	case reflect.Map:
+		iter := r.MapRange()
+		for iter.Next() {
+			// map key must be a string
+			if iter.Key().Kind() != reflect.String {
+				return "", fmt.Errorf("the key of the map type must be a string")
+			}
+
+			mapKey := iter.Key().String()
+			m := iter.Value()
+			if m.Kind() == reflect.Interface {
+				if err := e.encode(mapKey, reflect.ValueOf((m.Interface()))); err != nil {
+					return "", err
+				}
+				continue
+			}
+			if err := e.encode(mapKey, m); err != nil {
+				return "", err
+			}
+		}
+	default:
+		return "", fmt.Errorf("type %s is not available", r.Kind().String())
+	}
+
+	if len(e.v) == 0 {
 		return "", nil
 	}
-
-	v := values{
-		v: url.Values{},
-	}
-	for key, value := range *q {
-		r := reflect.ValueOf(value)
-		if err := v.add(key, r); err != nil {
-			return "", err
-		}
-	}
-
-	return "?" + v.v.Encode(), nil
+	return "?" + e.v.Encode(), nil
 }
 
-type values struct {
+type encoder struct {
 	v url.Values
 }
 
-func (v *values) add(key string, r reflect.Value) (err error) {
+func (e *encoder) encode(key string, r reflect.Value) (err error) {
 	switch r.Kind() {
 	case reflect.Bool:
-		v.v.Add(key, fmt.Sprintf("%v", r.Bool()))
+		e.v.Add(key, fmt.Sprintf("%v", r.Bool()))
 	case reflect.Int, reflect.Int64, reflect.Int32, reflect.Int16, reflect.Int8:
-		v.v.Add(key, fmt.Sprintf("%d", r.Int()))
+		e.v.Add(key, fmt.Sprintf("%d", r.Int()))
 	case reflect.Uint, reflect.Uint64, reflect.Uint32, reflect.Uint16, reflect.Uint8:
-		v.v.Add(key, fmt.Sprintf("%d", r.Uint()))
+		e.v.Add(key, fmt.Sprintf("%d", r.Uint()))
 	case reflect.Float32:
-		v.v.Add(key, fmt.Sprintf("%v", float32(r.Float())))
+		e.v.Add(key, fmt.Sprintf("%v", float32(r.Float())))
 	case reflect.Float64:
-		v.v.Add(key, fmt.Sprintf("%v", r.Float()))
+		e.v.Add(key, fmt.Sprintf("%v", r.Float()))
 	case reflect.Map:
-		err = v.addMap(key, r)
+		err = e.encodeMap(key, r)
 	case reflect.Array, reflect.Slice:
-		err = v.addArray(key, r)
+		err = e.encodeArray(key, r)
 	case reflect.String:
-		v.v.Add(key, r.String())
+		e.v.Add(key, r.String())
 	default:
 		err = fmt.Errorf("type %s is not available (key: %s)", r.Kind().String(), key)
 	}
 	return err
 }
 
-func (v *values) addMap(key string, r reflect.Value) error {
+func (e *encoder) encodeMap(key string, r reflect.Value) error {
 	iter := r.MapRange()
 	for iter.Next() {
 		// map key must be a string
@@ -75,29 +94,29 @@ func (v *values) addMap(key string, r reflect.Value) error {
 		mapKey := key + "[" + iter.Key().String() + "]"
 		m := iter.Value()
 		if m.Kind() == reflect.Interface {
-			if err := v.add(mapKey, reflect.ValueOf((m.Interface()))); err != nil {
+			if err := e.encode(mapKey, reflect.ValueOf((m.Interface()))); err != nil {
 				return err
 			}
 			continue
 		}
-		if err := v.add(mapKey, m); err != nil {
+		if err := e.encode(mapKey, m); err != nil {
 			return err
 		}
 	}
 	return nil
 }
 
-func (v *values) addArray(key string, r reflect.Value) error {
+func (e *encoder) encodeArray(key string, r reflect.Value) error {
 	for i := 0; i < r.Len(); i++ {
 		arrKey := fmt.Sprintf("%s[%d]", key, i)
 		s := r.Index(i)
 		if s.Kind() == reflect.Interface {
-			if err := v.add(arrKey, reflect.ValueOf((s.Interface()))); err != nil {
+			if err := e.encode(arrKey, reflect.ValueOf((s.Interface()))); err != nil {
 				return err
 			}
 			continue
 		}
-		if err := v.add(arrKey, s); err != nil {
+		if err := e.encode(arrKey, s); err != nil {
 			return err
 		}
 	}
--- encoder_test.go
+++ encoder_test.go
@@ -61,7 +61,7 @@ func TestQ_Encode(t *testing.T) {
 
 		for _, tc := range testCases {
 			t.Run(tc.name, func(t *testing.T) {
-				a, err := tc.q.Encode()
+				a, err := qstringer.Encode(tc.q)
 				if err != nil {
 					t.Fatalf("Encode() should not returns error: %s", err)
 				}
@@ -77,25 +77,52 @@ func TestQ_Encode(t *testing.T) {
 
 		testCases := []struct {
 			name string
-			q    qstringer.Q
+			q    interface{}
 			err  error
 		}{
-			// map key is not string type
-
 			// unavailable types
-			{name: "uintptr type", q: qstringer.Q{"key": uintptr(1)}, err: fmt.Errorf("type uintptr is not available (key: key)")},
-			{name: "complex64 type", q: qstringer.Q{"key": complex64(1)}, err: fmt.Errorf("type complex64 is not available (key: key)")},
-			{name: "complex128 type", q: qstringer.Q{"key": complex128(1)}, err: fmt.Errorf("type complex128 is not available (key: key)")},
-			{name: "chan type", q: qstringer.Q{"key": make(chan int)}, err: fmt.Errorf("type chan is not available (key: key)")},
-			{name: "func type", q: qstringer.Q{"key": func() {}}, err: fmt.Errorf("type func is not available (key: key)")},
-			{name: "ptr type", q: qstringer.Q{"key": func() *string { return nil }()}, err: fmt.Errorf("type ptr is not available (key: key)")},
-			{name: "struct type", q: qstringer.Q{"key": struct{}{}}, err: fmt.Errorf("type struct is not available (key: key)")},
-			{name: "unsafe pointer type", q: qstringer.Q{"key": unsafe.Pointer(&v)}, err: fmt.Errorf("type unsafe.Pointer is not available (key: key)")},
+			{name: "bool type", q: true, err: fmt.Errorf("type bool is not available")},
+			{name: "int type", q: int(123), err: fmt.Errorf("type int is not available")},
+			{name: "int64 type", q: int64(123), err: fmt.Errorf("type int64 is not available")},
+			{name: "int32 type", q: int32(123), err: fmt.Errorf("type int32 is not available")},
+			{name: "int16 type", q: int16(123), err: fmt.Errorf("type int16 is not available")},
+			{name: "int8 type", q: int8(123), err: fmt.Errorf("type int8 is not available")},
+			{name: "uint type", q: uint(123), err: fmt.Errorf("type uint is not available")},
+			{name: "uint64 type", q: uint64(123), err: fmt.Errorf("type uint64 is not available")},
+			{name: "uint32 type", q: uint32(123), err: fmt.Errorf("type uint32 is not available")},
+			{name: "uint16 type", q: uint16(123), err: fmt.Errorf("type uint16 is not available")},
+			{name: "uint8 type", q: uint8(123), err: fmt.Errorf("type uint8 is not available")},
+			{name: "float64 type", q: float64(123.456), err: fmt.Errorf("type float64 is not available")},
+			{name: "float32 type", q: float32(123.456), err: fmt.Errorf("type float32 is not available")},
+			{name: "string type", q: "hoge", err: fmt.Errorf("type string is not available")},
+			{name: "array type", q: [3]string{"1", "2", "3"}, err: fmt.Errorf("type array is not available")},
+			{name: "slice type", q: []string{"1", "2", "3"}, err: fmt.Errorf("type slice is not available")},
+			{name: "uintptr type", q: uintptr(1), err: fmt.Errorf("type uintptr is not available")},
+			{name: "complex64 type", q: complex64(1), err: fmt.Errorf("type complex64 is not available")},
+			{name: "complex128 type", q: complex128(1), err: fmt.Errorf("type complex128 is not available")},
+			{name: "chan type", q: make(chan int), err: fmt.Errorf("type chan is not available")},
+			{name: "func type", q: func() {}, err: fmt.Errorf("type func is not available")},
+			{name: "ptr type", q: func() *string { return nil }(), err: fmt.Errorf("type ptr is not available")},
+			{name: "struct type", q: struct{}{}, err: fmt.Errorf("type struct is not available")},
+			{name: "unsafe pointer type", q: unsafe.Pointer(&v), err: fmt.Errorf("type unsafe.Pointer is not available")},
+
+			// unavailable map value types
+			{name: "map vlalue of uintptr type", q: qstringer.Q{"key": uintptr(1)}, err: fmt.Errorf("type uintptr is not available (key: key)")},
+			{name: "map vlalue of complex64 type", q: qstringer.Q{"key": complex64(1)}, err: fmt.Errorf("type complex64 is not available (key: key)")},
+			{name: "map vlalue of complex128 type", q: qstringer.Q{"key": complex128(1)}, err: fmt.Errorf("type complex128 is not available (key: key)")},
+			{name: "map vlalue of chan type", q: qstringer.Q{"key": make(chan int)}, err: fmt.Errorf("type chan is not available (key: key)")},
+			{name: "map vlalue of func type", q: qstringer.Q{"key": func() {}}, err: fmt.Errorf("type func is not available (key: key)")},
+			{name: "map vlalue of ptr type", q: qstringer.Q{"key": func() *string { return nil }()}, err: fmt.Errorf("type ptr is not available (key: key)")},
+			{name: "map vlalue of struct type", q: qstringer.Q{"key": struct{}{}}, err: fmt.Errorf("type struct is not available (key: key)")},
+			{name: "map vlalue of unsafe pointer type", q: qstringer.Q{"key": unsafe.Pointer(&v)}, err: fmt.Errorf("type unsafe.Pointer is not available (key: key)")},
+
+			// map key is not string type
+			{name: "uintptr type", q: map[int]string{100: "val"}, err: fmt.Errorf("the key of the map type must be a string")},
 		}
 
 		for _, tc := range testCases {
 			t.Run(tc.name, func(t *testing.T) {
-				a, err := tc.q.Encode()
+				a, err := qstringer.Encode(tc.q)
 				if err == nil {
 					t.Fatalf("Encode() should returns error")
 				}
